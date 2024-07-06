e7e284dbb97cd312ddd78d5bae709419b3ecf26a
Seohyun Lee <seohyun.lee@ringleplus.com>
2024-07-06 10:36:51 +0900

    feat: implement chat history

--- frontend/src/lib/ChatBox.svelte
+++ frontend/src/lib/ChatBox.svelte
@@ -1,12 +1,83 @@
 <script lang="ts">
-    import {Icon, Input} from "nunui";
+    import {Button, CircularProgress, Icon, Input} from "nunui";
+    import Expand from "$lib/Expand.svelte";
+    import {tick} from "svelte";
+
+    async function getAnswer(question: string): Promise<string> {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        return '실망을 드린';
+    }
+
+    function ask() {
+        if (!value) return;
+        let ask = value;
+        value = '';
+        showHistory = true;
+        const arr = [ask];
+        history = [...history, arr];
+        scrollToBottom();
+        getAnswer(ask).then(answer => {
+            arr[1] = answer;
+            history = [...history];
+            scrollToBottom();
+        });
+    }
+
+    async function scrollToBottom() {
+        await tick();
+        await new Promise(resolve => setTimeout(resolve, 10));
+        container.scrollTop = container.scrollHeight;
+    }
+
+    let focus = false, value = '', showHistory = false, container: HTMLElement;
+    let history = [['안녕하세요', '저는 케인입니다'], ['먼저 저의 말과', '행동으로 인해']];
+
+    $: {
+        let _ = [showHistory];
+        scrollToBottom();
+    }
 </script>
 
 <main>
+    <Expand>
+        {#if true}
+            <div class="row" style="padding-bottom: 0">
+                <div style="width: 24px"></div>
+                <Button small outlined>전체 파일 참고</Button>
+                <Button small outlined={!showHistory} on:click={() => showHistory = !showHistory} icon="history">대화 내역
+                </Button>
+            </div>
+        {/if}
+    </Expand>
+    <Expand>
+        {#if showHistory}
+            <div class="container" bind:this={container}>
+                {#each history as [q, a]}
+                    <div class="row">
+                        <div style="width: 24px"></div>
+                        <div style="width: 100%">
+                            <div class="question">{q}</div>
+                            <div class="answer">
+                                <div style="margin-bottom: 12px">
+                                    <Icon auto_awesome/>
+                                </div>
+                                {#if a}
+                                    {a}
+                                {:else}
+                                    <CircularProgress indeterminate size="24"/>
+                                {/if}
+                            </div>
+                        </div>
+                    </div>
+                {/each}
+            </div>
+        {/if}
+    </Expand>
     <div class="row">
         <Icon auto_awesome style="margin-right: 12px"/>
         <div class="input">
-            <Input plain round fullWidth placeholder="아무거나 물어보세요!" trailingIcon="input"/>
+            <Input plain round fullWidth placeholder="아무거나 물어보세요!" trailingIcon="input" on:focus={() => focus = true}
+                   on:blur={() => setTimeout(() => focus = false, 10)} bind:value on:submit={ask}/>
         </div>
     </div>
 </main>
@@ -18,8 +89,8 @@
     left: var(--nav);
     width: calc(100vw - var(--nav) - 48px);
     margin: 24px;
-    background: var(--primary-light3);
-    border-radius: 100px;
+    background: var(--primary-light1);
+    border-radius: 36px;
 
     display: flex;
     flex-direction: column;
@@ -28,7 +99,7 @@
   .input {
     flex: 1;
     border-radius: 100px;
-    background: var(--primary-light1);
+    background: var(--primary-light2);
   }
 
   .row {
@@ -36,5 +107,32 @@
     align-items: center;
     padding: 12px;
     width: calc(100% - 24px);
+
+    & > :global(*) {
+      margin-right: 12px !important;
+    }
+  }
+
+  .question {
+    background: var(--secondary-light2);
+    margin-left: auto;
+    padding: 12px;
+    border-radius: 12px;
+    margin-bottom: 12px;
+    width: fit-content;
+  }
+
+  .answer {
+    background: var(--secondary-light4);
+    padding: 12px;
+    border-radius: 12px;
+    margin-bottom: 12px;
+    width: fit-content;
+  }
+
+  .container {
+    overflow-y: auto;
+    max-height: 50vh;
+    scroll-behavior: smooth;
   }
 </style>
---
+++ frontend/src/lib/Expand.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import {tweened} from "svelte/motion";
+    import {cubicInOut} from "svelte/easing";
+
+    export let x = false, y = !x, style = "";
+    let clientHeight = 0, clientWidth = 0, height = tweened(0, {duration: 300, easing: cubicInOut}),
+        width = tweened(0, {duration: 300, easing: cubicInOut});
+
+    $: if (y) height.set(clientHeight);
+    $: if (x) width.set(clientWidth);
+</script>
+
+<main style="{style};{x ? `width: ${$width}px;` : ''}{y ? `height: ${$height}px;` : ''}">
+    <div bind:clientHeight bind:clientWidth class:x class:y>
+        <slot/>
+    </div>
+</main>
+
+<style lang="scss">
+  main {
+    div {
+      position: absolute;
+
+      &.y {
+        width: 100%;
+      }
+
+      &.x {
+        height: 100%;
+      }
+    }
+
+    overflow: hidden;
+    position: relative;
+  }
+</style>
