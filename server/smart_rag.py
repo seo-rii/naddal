455355211914f5958539ad8386fb293d8952592d
Seohyun Lee <seohyun.lee@ringleplus.com>
2024-07-06 15:03:16 +0900

    fix: make env

--- server/smart_rag.py
+++ server/smart_rag.py
@@ -5,7 +5,7 @@ from tavily import TavilyClient
 def smart_rag(chain, question, context):
 
     print("Searching in tavily")
-    tavily = TavilyClient(api_key=api_key2)
+    tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
     context = tavily.search(query=question)
 
     return chain.invoke({"context": context, "question": question}).content
