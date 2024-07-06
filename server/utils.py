62182b9bc3bdc2882df6a2c9964c8777cb0a4a8f
Dongjae Lee <58449555+duncan020313@users.noreply.github.com>
2024-07-06 11:57:48 +0900

    implement tag remover

--- server/utils.py
+++ server/utils.py
@@ -5,12 +5,19 @@ from langchain_upstage import UpstageEmbeddings
 from langchain_upstage import UpstageLayoutAnalysisLoader
 from langchain.docstore.document import Document
 from langchain_upstage import ChatUpstage
-from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
-from langchain_text_splitters import (
-    Language,
-    RecursiveCharacterTextSplitter
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    SystemMessagePromptTemplate,
 )
+from langchain_text_splitters import Language, RecursiveCharacterTextSplitter
+import re
 
+
+def tag_remover(html_content: str) -> str:
+    """Remove html tags and extract only contents"""
+    tag_regex = r"<[^>]*>"
+    return re.sub(tag_regex, "", html_content)
 
 
 def pdf_to_html(pdf_filepath):
@@ -18,13 +25,14 @@ def pdf_to_html(pdf_filepath):
     docs = layzer.load()
     return docs
 
+
 # generate function for generate embedding
 def generate_embeddings(pdf_filepath, embedding_name):
     """
     Generate Embeddings for the given pdf, return 1 if success otherwise return 0
     """
     load_dotenv(find_dotenv())
-    api_key = os.getenv('UPSTAGE_API_KEY')
+    api_key = os.getenv("UPSTAGE_API_KEY")
     try:
         layzer = UpstageLayoutAnalysisLoader(pdf_filepath, output_type="html")
         docs = layzer.load()
@@ -33,15 +41,18 @@ def generate_embeddings(pdf_filepath, embedding_name):
             chunk_size=1000, chunk_overlap=100, language=Language.HTML
         )
         splits = text_spliiter.split_documents(docs)
-        unique_splits=[]
+        splits = [tag_remover(split.page_content) for split in splits]
+        unique_splits = []
         for split in splits:
             if split not in unique_splits:
                 unique_splits.append(split)
-        persist_directory=f"./chroma_db/{embedding_name}/"
+        persist_directory = f"./chroma_db/{embedding_name}/"
         vectorstore = Chroma.from_documents(
             documents=unique_splits,
             ids=[doc.page_content for doc in unique_splits],
-            embedding=UpstageEmbeddings(model="solar-embedding-1-large", api_key=api_key),
+            embedding=UpstageEmbeddings(
+                model="solar-embedding-1-large", api_key=api_key
+            ),
             persist_directory=persist_directory,
         )
         print(f"embedding saved in ./chroma_db/{embedding_name}/")
@@ -59,17 +70,18 @@ def inference(question, embedding_names):
     - embedding_names (List[str]): embedding names that the retriever should use it
     """
     load_dotenv(find_dotenv())
-    api_key = os.getenv('UPSTAGE_API_KEY')
+    api_key = os.getenv("UPSTAGE_API_KEY")
+
     def retrieve(retriever, input_query):
         return retriever.invoke(input_query)
 
     def get_retriever(embedding_name):
         return Chroma(
             persist_directory=f"./chroma_db/{embedding_name}/",
-            embedding_function=UpstageEmbeddings(model="solar-embedding-1-large")
+            embedding_function=UpstageEmbeddings(model="solar-embedding-1-large"),
         ).as_retriever()
-    
-    name_list = os.listdir('./chroma_db/')
+
+    name_list = os.listdir("./chroma_db/")
     embedding_names = list(set(embedding_names))
     # check if their are any invalid names of embeddings
     print("[[CHECKING EMBEDDING NAMES...]]")
@@ -79,23 +91,23 @@ def inference(question, embedding_names):
 
     print("[[LOADING EMBEDDINGS...]]")
     # accumulate retrivers into a single list
-    retrievers=[]
+    retrievers = []
     for name in embedding_names:
         retriever = get_retriever(name)
         print(retriever)
         retrievers.append((retriever, name))
     # generate documents
     print("[[RETRIEVING RELEVANT DOCS...]]")
-    context=""
+    context = ""
     for retriever in retrievers:
         result = retrieve(retriever[0], question)
         print(result)
-        #TODO post process result
+        # TODO post process result
         # - html 정제하기
         # - 순수 text 뽑기
-        refined_result=""+f"from [{retriever[1]}]"
-        context+=refined_result+"\n\n"
-    
+        refined_result = "" + f"from [{retriever[1]}]"
+        context += refined_result + "\n\n"
+
     system_msg = SystemMessagePromptTemplate.from_template(
         "You are an assistant for question-answering tasks.\n"
         "Use the following pieces of retrieved context to answer the question.\n"
@@ -106,14 +118,10 @@ def inference(question, embedding_names):
         "Use three sentences maximum and keep the answer concise.\n"
     )
     human_msg = HumanMessagePromptTemplate.from_template(
-    "Question: {question}\n\n" 
-    "Context: {context}" 
-    "Answer:"
+        "Question: {question}\n\n" "Context: {context}" "Answer:"
     )
-    chat_prompt = ChatPromptTemplate.from_messages(
-        [system_msg, human_msg]
-    )
+    chat_prompt = ChatPromptTemplate.from_messages([system_msg, human_msg])
     model = ChatUpstage(api_key=api_key)
     chain = chat_prompt | model
-    output = chain.invoke({'question': question, 'context': context})
+    output = chain.invoke({"question": question, "context": context})
     return output.content
